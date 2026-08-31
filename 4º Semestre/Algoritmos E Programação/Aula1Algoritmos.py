b8e616b07c0f03e790e73a7b78ff0f1a654b39fc
Caue Finger <fingercaue@gmail.com>
2026-08-31 20:23:25 -0300

    início da cadeira de algoritmos de programação em python

---
+++ 4º Semestre/Algoritmos E Programação/Aula1Algoritmos.py
@@ -0,0 +1,10 @@
+nome = input("Digite seu nome: ")
+
+print(f"Olá, {nome}! Esse é o meu primeiro código com o Gastão")
+
+
+nome = input("Digite seu nome: ")
+idade = int(input("Digite sua idade: "))
+
+
+apresentacao = print(f"Você tem {nome} e {idade} anos para iniciar a programação. ")
