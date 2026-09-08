fa23d1f7229b4de461de5a74e6d9c9c91b854667
Caue Finger <fingercaue@gmail.com>
2026-09-08 15:20:06 -0300

    cupons criados, lista de compras do carrinho criada

--- 4º Semestre/Programação Para Web/EXERCÍCIO AULA 5/main.js
+++ 4º Semestre/Programação Para Web/EXERCÍCIO AULA 5/main.js
@@ -9,7 +9,25 @@ const carrinho = [
 ];
 
 for (const item of carrinho){
-    console.log (item.nome),
+    const valor_total = item.quantidade * item.preco;
+    console.log(item.nome),
     console.log(item.preco),
-    console.log(item.quantidade)
+    console.log(item.quantidade),
+    console.log(valor_total)
+}
+
+
+function Cupom_10(valor_total) {
+    let DESCONTO10 = valor_total - (valor_total * 0.10)
+    print(`Parabéns! Sua compra recebeu 10% de desconto. Seu valor total é ${DESCONTO10}`)
+}
+
+function Cupom_20(valor_total) {
+    let DESCONTO20 = valor_total - (valor_total * 0.20)
+    print(`Parabéns! Sua compra recebeu 20% de desconto. Seu valor total é ${DESCONTO20}`)
+}
+
+function Cupom_default(valor_total) {
+    let Cupom_default = valor_total 
+    print(`Seu valor total é ${valor_total}`)
 }
