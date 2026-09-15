5793d4dba27187178bf87596fc51542acaccb385
Caue Finger <fingercaue@gmail.com>
2026-09-15 20:26:51 -0300

    meu primeiro código em react e estrutura criada pelo npm

---
+++ 4º Semestre/Programação Para Web/meu-primeiro-react/src/App.jsx
@@ -0,0 +1,7 @@
+function App() {
+    return (
+        <h1>Olá! Esse é meu primeiro programa em React!</h1>
+    );
+}
+
+export default App;
---
+++ 4º Semestre/Programação Para Web/teste react/src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+    return (
+        <h1>
+            Olá! Esse é meu primeiro programa utilizando React com HTML.
+        </h1>
+    );
+}
+
+export default App;
+
+// App é o componente principal da aplicação React.
+// Ele retorna o JSX que será exibido na interface.
